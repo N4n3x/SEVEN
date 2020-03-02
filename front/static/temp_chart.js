142e5fcedbe23b69f5e4fef4541660302ca27438
N4n3x <alex.hern.dev@gmail.com>
2020-03-02 22:07:39 +0100

    finis ;)

--- front/static/temp_chart.js
+++ front/static/temp_chart.js
@@ -4,7 +4,7 @@ class Graph_temp {
         this.get_data().then((rep) => {
             console.log(rep);
             this.data = this.format_data(rep);
-            // this.chart = this.disp_chars(this.data);
+            this.chart = this.disp_chars(this.data);
         });
     }
 
@@ -20,38 +20,39 @@ class Graph_temp {
         };
         const response = await fetch(this.url, myInit);
         const myJson = await response.json();
-        console.log(myJson);
+        // console.log(myJson);
         return myJson
     }
 
     format_data(e) {
-        console/log(e);
-        // var labels = e.id_station.map(function (val) {
-        //     return val.date;
-        // });
-        // var data_time = e.timestamp.map(function (val) {
-        //     return val.eur;
-        // });
-        // var data_temp = e.temperature.map(function (val) {
-        //     return val.usd;
-        // });
 
-        // return [labels, data_time, data_temp]
+        var labels = e.map(function (val) {
+            return moment.unix(val.timestamp).format("DD/MM/YYYY HH:mm");
+        });
+        var data_time = e.map(function (val) {
+            return val.temperature;
+        });
+        var data_temp = e.map(function (val) {
+            return val.humidite;
+        });
+
+        return [labels, data_time, data_temp]
     }
 
     disp_chars(data) {
+        console.log(data);
         var ctx = document.getElementById('myChart').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'line',
             data: {
                 labels: data[0],
                 datasets: [{
-                    label: "EUR",
+                    label: "Température",
                     data: data[1],
                     borderColor: 'rgb(250,130,20)',
                     borderWidth: 1
                 }, {
-                    label: "USD",
+                    label: "Humidité",
                     data: data[2],
                     borderColor: 'rgb(40,130,200)',
                     borderWidth: 1
